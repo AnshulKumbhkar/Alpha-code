f5a1aff3162cef75ab922d2099ece97727e98423
Anshul Kumbhkar <anshulkumbhkar2005@gmail.com>
2026-09-12 12:57:58 +0530

    added trpc

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono, Inter } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
+import { TRPCReactProvider } from "@/trpc/client";
 
 const inter = Inter({subsets:['latin'],variable:'--font-sans'});
 
@@ -22,11 +23,13 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: LayoutProps<"/">) {
   return (
+    <TRPCReactProvider>
     <html
       lang="en"
       className={cn("h-full", "antialiased", geistSans.variable, geistMono.variable, "font-sans", inter.variable)}
     >
       <body className="min-h-full flex flex-col">{children}</body>
     </html>
+    </TRPCReactProvider>
   );
 }
